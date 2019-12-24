de98c2b835ac44119873ccf9de0f177bc2bf803e
Jinyang79 <827126732@qq.com>
2019-12-24 20:23:28 +0800

    完成了添加事项，删除事项，剩余条数

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <a-input placeholder="请输入任务"
-             class="my_ipt" />
-    <a-button type="primary">添加事项</a-button>
+             class="my_ipt"
+             :value="inputValue"
+             @change="handleInputChange" />
+    <a-button type="primary"
+              @click="addItemToList">添加事项</a-button>
 
     <a-list bordered
             :dataSource="list"
@@ -10,16 +13,18 @@
       <a-list-item slot="renderItem"
                    slot-scope="item">
         <!-- 复选框 -->
-        <a-checkbox>{{item.info}}</a-checkbox>
+        <a-checkbox :checked="item.done"
+                    @change="(e)=>{cbChange(e,item.id)}">{{item.info}}</a-checkbox>
         <!-- 删除链接 -->
-        <a slot="actions">删除</a>
+        <a slot="actions"
+           @click="removeItemById(item.id)">删除</a>
       </a-list-item>
 
       <!-- footer区域 -->
       <div slot="footer"
            class="footer">
         <!-- 未完成的任务个数 -->
-        <span>0条剩余</span>
+        <span>{{unDoneLength}}条剩余</span>
         <!-- 操作按钮 -->
         <a-button-group>
           <a-button type="primary">全部</a-button>
@@ -34,25 +39,43 @@
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
 export default {
   name: 'app',
   data () {
     return {
-      list: [
-        {
-          id: 0,
-          info: 'Racing car sprays burning fuel into crowd.',
-          done: false
-        },
-        { id: 1, info: 'Japanese princess to wed commoner.', done: false },
-        {
-          id: 2,
-          info: 'Australian walks 100km after outback crash.',
-          done: false
-        },
-        { id: 3, info: 'Man charged over missing wedding girl.', done: false },
-        { id: 4, info: 'Los Angeles battles huge wildfires.', done: false }
-      ]
+    }
+  },
+  created () {
+    this.$store.dispatch('getList')
+  },
+  computed: {
+    ...mapState(['list', 'inputValue']),
+    ...mapGetters(['unDoneLength'])
+
+  },
+  methods: {
+    handleInputChange (e) {
+      this.$store.commit('setInputValue', e.target.value)
+    },
+    addItemToList () {
+      if (this.inputValue.trim().length <= 0) {
+        // ant ui提供$message
+        return this.$message.warning('文本框不为空')
+      }
+      this.$store.commit('addItem')
+    },
+    removeItemById (id) {
+      this.$store.commit('removeItem', id)
+    },
+    cbChange (e, id) {
+      /* console.log(e.target.checked) */
+      /*  console.log(id) */
+      const param = {
+        id: id,
+        status: e.target.checked
+      }
+      this.$store.commit('changStatus', param)
     }
   }
 }
